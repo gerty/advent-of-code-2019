32f18089c590c25754743aea6059fc73904a467f
gerty <gerty33@gmail.com>
2019-12-03 01:49:18 -0600

    Part 2 of Day 3 complete. Modified existing Part 1 code. The real learning will come when I optimize.

--- AoC2019-Day3.py
+++ AoC2019-Day3.py
@@ -7,67 +7,104 @@
 with open('Day03-INPUT.txt', 'r') as f:
     filedata = f.readlines()
 
-# data = []
+data = []
 gridsize = 20000
 origin = int(gridsize / 2);
-grid = [[0 for i in range(gridsize)] for j in range(gridsize)] # bits in grid locations will correspond to wire crossed
+grid = [[0 for i in range(gridsize)] for j in range(gridsize)]  # bits in grid locations will correspond to crossing
 wirecode = 0  # keep track of which wire/bit we're tracking
 minDistance = gridsize * 2
 
-for line in filedata:
-    data = line.split()
-    for wire in data:
-        print(wire)
-        wirecode += 1
-        print(wirecode)
-        x = origin  # put the cursor at the origin (defined above as center-ish of grid) for each wire
-        y = origin
-        nav = wire.split(',')  # split commands by commas
-        print(nav)
-        for navcom in nav:
-            if navcom[0] == 'U':  # if the first letter of the navcom is an "UP" command
-                for i in range(int(navcom[1:])):  # the remainder of the navcom is a distance
-                    y += 1
-                    if not (int(grid[x][y]) & wirecode):  # if wirecode bit not flipped yet
-                        grid[x][y] ^= wirecode
-                        if grid[x][y] == 3:  # replace with full & of max wirecode if problem goes in that direction...
-                            print('CROSSED at x:' + str(x) + " y:" + str(y) + " at distance" + str(x+y))
-                            if abs(x-origin) + abs(y-origin) < minDistance:  # calculate Manhattan distance
-                                minDistance = abs(x-origin) + abs(y-origin)
-                                print(minDistance)
-            if navcom[0] == 'D':  # if the first letter of the navcom is an "UP" command
-                for i in range(int(navcom[1:])):  # the remainder of the navcom is a distance
-                    y -= 1
-                    if (not (grid[x][y] & wirecode)):  # if wirecode bit not flipped yet
-                        grid[x][y] ^= wirecode
-                        if grid[x][y] == 3:  # replace with full & of max wirecode if problem goes in that direction...
-                            print('CROSSED at x:' + str(x) + " y:" + str(y) + " at distance" + str(x+y))
-                            if abs(x-origin) + abs(y-origin) < minDistance:  # calculate Manhattan distance
-                                minDistance = abs(x-origin) + abs(y-origin)
-                                print(minDistance)
-            if navcom[0] == 'R':  # if the first letter of the navcom is an "UP" command
-                for i in range(int(navcom[1:])):  # the remainder of the navcom is a distance
-                    x += 1
-                    if (not (grid[x][y] & wirecode)):  # if wirecode bit not flipped yet
-                        grid[x][y] ^= wirecode
-                        if grid[x][y] == 3:  # replace with full & of max wirecode if problem goes in that direction...
-                            print('CROSSED at x:' + str(x) + " y:" + str(y) + " at distance" + str(x+y))
-                            if abs(x-origin) + abs(y-origin) < minDistance:  # calculate Manhattan distance
-                                minDistance = abs(x-origin) + abs(y-origin)
-                                print(minDistance)
-            if navcom[0] == 'L':  # if the first letter of the navcom is an "UP" command
-                for i in range(int(navcom[1:])):  # the remainder of the navcom is a distance
-                    x -= 1
-                    if (not (grid[x][y] & wirecode)):  # if wirecode bit not flipped yet
-                        grid[x][y] ^= wirecode  # flip a bit associated with that wirecode
-                        if grid[x][y] == 3:  # replace with full & of max wirecode if problem goes in that direction...
-                            print('CROSSED at x:' + str(x) + " y:" + str(y) + " at distance" + str(x+y))
-                            if abs(x-origin) + abs(y-origin) < minDistance:  # calculate Manhattan distance
-                                minDistance = abs(x-origin) + abs(y-origin)
-                                print(minDistance)
+wire = filedata[0]
+print(wire)
+wirecode += 1
+print(wirecode)
+x = origin  # put the cursor at the origin (defined above as center-ish of grid) for each wire
+y = origin
+nav = wire.split(',')  # split commands by commas
+print(nav)
+movecount = 0
+
+for navcom in nav:
+    if navcom[0] == 'U':  # if the first letter of the navcom is an "UP" command
+        for i in range(int(navcom[1:])):  # the remainder of the navcom is a distance
+            y += 1  # go up
+            movecount += 1
+            if not (grid[x][y]):  # if the point has not been passed (only record first pass)
+                grid[x][y] = movecount  # leave a trail of movecounts
+
+    if navcom[0] == 'D':  # if the first letter of the navcom is an "UP" command
+        for i in range(int(navcom[1:])):  # the remainder of the navcom is a distance
+            y -= 1  # go down
+            movecount += 1
+            if not (grid[x][y]):  # if the point has not been passed
+                grid[x][y] = movecount  # leave a trail of movecounts
+
+    if navcom[0] == 'R':  # if the first letter of the navcom is an "UP" command
+        for i in range(int(navcom[1:])):  # the remainder of the navcom is a distance
+            x += 1  # go right
+            movecount += 1
+            if not (grid[x][y]):  # if the point has not been passed
+                grid[x][y] = movecount  # leave a trail of movecounts
+
+    if navcom[0] == 'L':  # if the first letter of the navcom is an "UP" command
+        for i in range(int(navcom[1:])):  # the remainder of the navcom is a distance
+            x -= 1  # go left
+            movecount += 1
+            if not (grid[x][y]):  # if the point has not been passed
+                grid[x][y] = movecount  # leave a trail of movecounts
+
+wire = filedata[1]
+print(wire)
+wirecode += 1
+print(wirecode)
+x = origin  # put the cursor at the origin (defined above as center-ish of grid) for each wire
+y = origin
+nav = wire.split(',')  # split commands by commas
+print(nav)
+movecount = 0
+
+for navcom in nav:
+    if navcom[0] == 'U':  # if the first letter of the navcom is an "UP" command
+        for i in range(int(navcom[1:])):  # the remainder of the navcom is a distance
+            y += 1
+            movecount += 1  # if previous pass did not contact point just count and move along
+            if (grid[x][y]):  # if record of a crossing at x,y then count it as a possible winner
+                print('CROSSED at x:' + str(x) + " y:" + str(y) + " with moves: " + str(movecount + grid[x][y]))
+                if movecount + grid[x][y] < minDistance:  # calculate Manhattan distance
+                    minDistance = movecount + grid[x][y]
+                    print(movecount + grid[x][y])
+    if navcom[0] == 'D':  # if the first letter of the navcom is an "UP" command
+        for i in range(int(navcom[1:])):  # the remainder of the navcom is a distance
+            y -= 1
+            movecount += 1  # if previous pass did not contact point just count and move along
+            if (grid[x][y]):  # if record of a crossing at x,y then count it as a possible winner
+                print('CROSSED at x:' + str(x) + " y:" + str(y) + " with moves: " + str(movecount + grid[x][y]))
+                if movecount + grid[x][y] < minDistance:  # calculate Manhattan distance
+                    minDistance = movecount + grid[x][y]
+                    print(movecount + grid[x][y])
+    if navcom[0] == 'R':  # if the first letter of the navcom is an "UP" command
+        for i in range(int(navcom[1:])):  # the remainder of the navcom is a distance
+            x += 1
+            movecount += 1  # if previous pass did not contact point just count and move along
+            if (grid[x][y]):  # if record of a crossing at x,y then count it as a possible winner
+                print('CROSSED at x:' + str(x) + " y:" + str(y) + " with moves: " + str(movecount + grid[x][y]))
+                if movecount + grid[x][y] < minDistance:  # calculate Manhattan distance
+                    minDistance = movecount + grid[x][y]
+                    print(movecount + grid[x][y])
+    if navcom[0] == 'L':  # if the first letter of the navcom is an "UP" command
+        for i in range(int(navcom[1:])):  # the remainder of the navcom is a distance
+            x -= 1
+            movecount += 1  # if previous pass did not contact point just count and move along
+            if (grid[x][y]):  # if record of a crossing at x,y then count it as a possible winner
+                print('CROSSED at x:' + str(x) + " y:" + str(y) + " with moves: " + str(movecount + grid[x][y]))
+                if movecount + grid[x][y] < minDistance:  # calculate Manhattan distance
+                    minDistance = movecount + grid[x][y]
+                    print(movecount + grid[x][y])
 
 # At this point we should have two (or more) wires overlayed onto our grid.
 # Now we just need to find the closest intersecting point (all bits flipped) to our origin.
 # Or not, if the above integrated code works.
 print('DONE!')
 print(minDistance)
+
+# Not 12930 even when adding 2 to 12928 it is too low.
