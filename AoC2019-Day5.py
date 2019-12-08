23c4546e663057cf1c3af9542940a80f9189f2e9
gerty <gerty33@gmail.com>
2019-12-08 01:03:52 -0600

    Progress on Day 5. Will continue over breakfast.

--- AoC2019-Day5.py
+++ AoC2019-Day5.py
@@ -1,21 +1,50 @@
+# Code written by @gerty
+
+# Input from Day 5, Problem 1 (and 2) of Advent of Code 2019
+# Input files in same directory
+# My input: in file Day05-INPUT.txt
 
 
+def computer(memory, noun, verb, inputparam):
+    data = []  # create a list for use inside the function
+    done = False
+    for i in memory:
+        data.append(int(i))  # build local list "data" with only the integers
+    data[1] = int(noun)  # initialize the intcode program with predetermined initial values
+    data[2] = int(verb)  # all of data[] should be of type integer
+    address = 0
+
+    while not done:
+        if int(data[address]) == 1:
+            data[data[address+3]] = data[data[address+1]] + data[data[address+2]]
+            address += 4
+
+        if int(data[address]) == 2:
+            data[data[address+3]] = data[data[address+1]] * data[data[address+2]]
+            address += 4
+
+        if int(data[address]) == 3:
+            data[data[address+1]] = data[inputparam]
+            address += 2
+
+        if int(data[address]) == 4:
+            # outputparam = data[data[address+1]] # find a way to output param
+            print(data[data[address+1]])
+            address += 2
+
+        if int(data[address]) == 99:
+            return data[0]
+
+
+myData = []
+
+
+with open('Day02-INPUT.txt', 'r') as f:
+    filedata = f.readlines()
 for line in filedata:
-    data = line.split()
-    for item in data:
-        print(item)
+    myData = line.split(',')
 
-while not donehere:
-    if int(funcData[i + 0]) == 1:
-        result = int(funcData[int(funcData[i + 1])]) + int(funcData[int(funcData[i + 2])])
-        funcData[int(funcData[i + 3])] = result
-        i += 4
+print(myData)
+print(computer(myData, 12, 2, 1))
 
-    if int(funcData[i + 0]) == 2:
-        result = int(funcData[int(funcData[i + 1])]) * int(funcData[int(funcData[i + 2])])
-        funcData[int(funcData[i + 3])] = result
-        i += 4
-
-    if int(funcData[i + 0]) == 99:
-        print("The answer is: ")
-        return (int(funcData[0]))
+# Not 682644 (too low), not 7594646 (too high)
